416ddbacb7c88916de21f5d165d3d39c7a5927b6
VedanshN <vedansh.nema@outlook.com>
2025-11-11 17:18:29 +0530

    added initial endpoint for ml classifier

---
+++ app/api/ml.py
@@ -0,0 +1,20 @@
+from api.onboarding import get_onboarding_data
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from app.api.deps import get_current_user, get_async_db
+from app.db.models import User as DBUser, Onboarding as DBOnboarding
+from app.schemas import OnboardingCreate, User
+
+
+router = APIRouter()
+
+@router.get("/classifierdata", response_model=OnboardingCreate, tags=["ml"])
+async def get_classifier_data(
+    current_user: DBUser = Depends(get_current_user),
+    db: AsyncSession = Depends(get_async_db)
+):
+    onboarding_data = await get_onboarding_data(current_user.id, db)
+    if not onboarding_data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Onboarding data not found for the user.")
+    return onboarding_data   
